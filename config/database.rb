995f5c0a7b7fb424ed6920b07bc653ff411980a1
john <lijinghao333@aliyun.com>
2016-08-09 22:20:56 +0800

    m

--- config/database.rb
+++ config/database.rb
@@ -3,12 +3,10 @@ DataMapper::Property::String.length(255)
 require "redis"
 
 case Padrino.env
-  when :development then
-    DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_dev")
-    $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
-  when :production  then
-    DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_production")
-    $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+  when :development then DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_dev")
+  when :production  then DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_production")
 end
+
+$redis = Redis.new(:host => "127.0.0.1", :port => 6379)
 
 
