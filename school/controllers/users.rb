e373c3315b7a363648a1d58ee59810eaae9236aa
john <lijinghao333@aliyun.com>
2016-08-15 20:53:17 +0800

    m

--- school/controllers/users.rb
+++ school/controllers/users.rb
@@ -78,7 +78,7 @@ Tekala::School.controllers :v1, :users  do
       if @user.save
         render 'user'
       else
-        {:status => :failure, :msg => @user.errors.to_s}.to_json
+        {:status => :failure, :msg => @user.errors.to_h}.to_json
       end
     else
       {:status => :failure, :msg => '参数错误'}.to_json
