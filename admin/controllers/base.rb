ad2e986d211000fd4c5ab6cbd0dbbd3066f3cec9
john <lijinghao333@aliyun.com>
2016-08-18 16:20:49 +0800

    add base

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -2,17 +2,17 @@ Tekala::Admin.controllers :base do
 
   before do
     if session[:role_user_id]
-      @role_user = RoleUser.get session[:role_user_id]
-      @school_no = session[:school_no]
-      @mobile     = session[:mobile]
+      $role_user = RoleUser.get session[:role_user_id]
+      $school_no = session[:school_no]
+      $mobile     = session[:mobile]
+      $school = School.first(:school_no => @school_no)
     elsif !params['demo'].present?
       redirect_to(url(:edit_psd, :unlogin))
     end
   end
 
   get :index, :map => "/" do
-
-    @school  = School.first(:school_no => session[:school_no])
+    @school  = $school
     @user    = RoleUser.first(:id => session[:role_user_id])
     @car     = Car.all(:school_id => session[:school_id])
     @student = Student.all(:shop_id => session[:school_id])
@@ -20,16 +20,16 @@ Tekala::Admin.controllers :base do
     @shop    = Shop.all(:school_id => session[:school_id])
     @order   = Order.all(:school_id => session[:school_no])
 
-    school = School.first(:school_no => @school_no)
+
     month_beginning = Date.strptime(Time.now.beginning_of_month.to_s,'%Y-%m-%d')
 
     this_month = month_beginning  .. Date.tomorrow
-    @signups = school.signups.all(:pay_at => this_month, :status => 2).count
-    @amounts = school.signups.all(:pay_at => this_month, :status => 2).sum(:amount)
+    @signups = $school.signups.all(:pay_at => this_month, :status => 2).count
+    @amounts = $school.signups.all(:pay_at => this_month, :status => 2).sum(:amount)
 
     today = Date.today .. Date.tomorrow
-    @orders = school.users.orders.all(:book_time => today, :status => [3, 4]).count
-    @exams = school.users.cycles.all(:date => today).count
+    @orders = $school.users.orders.all(:book_time => today, :status => [3, 4]).count
+    @exams  = $school.users.cycles.all(:date => today).count
 
     render "index/index"
   end
