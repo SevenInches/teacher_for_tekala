1baef137cca7edd94e7df618d1ce1117faa05f8d
john <lijinghao333@aliyun.com>
2016-08-22 16:53:52 +0800

    m

--- school/controllers/cars.rb
+++ school/controllers/cars.rb
@@ -103,10 +103,8 @@ Tekala::School.controllers :v1, :cars  do
   delete :car, :map => '/v1/cars/:car_id', :provides => [:json] do
     car = Car.get(params[:car_id])
     if car.present?
-      if car.destroy
+      if car.check.destroy && car.destroy
         {:status => :success, :msg => '车辆删除成功'}.to_json
-      else
-        {:status => :success, :msg => car.errors.first.first }.to_json
       end
     else
       {:status => :failure, :msg => '车辆不存在'}.to_json
